7f2a88d2dd50c27985762fca16f4448156a16829
Likith R <70283754+likileads@users.noreply.github.com>
2021-12-12 19:12:43 +0530

    created stylesheet & Note.jsx and render the first note and its description onto the screen, curStatus:  working fine

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
 
 function App() {
     return (
-        <h1>Hello World</h1>
+        <div>
+            <Header />
+            <Note />
+            <Footer />
+        </div>
     );
 }
 
---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Note() {
+  return (
+    <div className="note">
+        <h1>Note1</h1>
+        <p>Note1's description goes here</p>
+    </div>
+    
+  );
+}
+
+export default Note;
